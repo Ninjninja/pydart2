6a95291b59aa27aefcc6a832f7d253d0d655bc49
Niranjan <kniranjankumar.eee@gmail.com>
2018-02-24 10:24:39 -0500

    Pushing with articulated joint

--- hello_pydart.py
+++ hello_pydart.py
@@ -7,35 +7,23 @@ import OpenGL.GLUT as GLUT
 from pydart2.gui.opengl.renderer import Renderer
 from pydart2.gui.trackball import Trackball
 from pydart2.gui.glut.window import GLUTWindow
+from pydart2.skeleton import Skeleton
+from pydart2.bodynode import BodyNode
 stop = False
 
-
-class ApplyForce:
+class DampingController(object):
     """ Add damping force to the skeleton """
-
     def __init__(self, skel):
         self.skel = skel
-        print("check")
-        self.count = 0
 
     def compute(self):
-        damping = skel.dq*0
-        # print (skel.dq)
-        # if skel.dq[1]>0.3:
-        #     skel.set_mobile(False)
-        self.count += 1
-        if self.count > 10:
-            print("check3")
-            if(np.linalg.norm(skel.dq>0.001)):
-                damping = -skel.dq*skel.bodynodes[0].mass()*0.4
-            else:
-                skel.set_velocities(skel.dq*0)
-        else:
-            #skel.set_mobile(True)
-           # print("check2")
-            damping[1] = 9.8
+        # print(self.skel.q)
+        damping = -self.skel.q *0
+        # damping[-1] = 10
+        # damping[1::3] *= 0.1
+        bod = skel.root_bodynode()
+        # bod.add_ext_force(np.array([0, 0, 2]), np.array([0, 0, 0]))
         return damping
-
 
 if __name__ == '__main__':
     print('Hello, PyDART!')
@@ -43,18 +31,14 @@ if __name__ == '__main__':
     pydart.init()
     print('pydart initialization OK')
 
-    world = pydart.World(0.0005, 'examples/data/skel/cube_data.skel')
-    #world.set_gravity([0, -9.8, 0])
-    print('pydart create_world OK')
-    # pydart.gui.viewer.launch(world)
+    world = pydart.World(0.0005,'examples/data/skel/cube_data.skel')
     skel = world.skeletons[-1]
-    skel.friction = 0.9
-    #skel.q = (np.random.rand(skel.ndofs))
-    #skel.set_forces(np.array([0, 0, 0, 100, 0, 0]))
-    #skel.set_accelerations([0, 5, 0])
-    #print(skel.tau)
-    #print(skel.friction)
-    skel.controller = ApplyForce(skel)
+    bod = skel.root_bodynode()
+    bod.add_ext_force(np.array([0, 0, 400]), np.array([0, 0, 0]))
+    # bod.add_ext_force(np.array([100, 0, 100]), np.array([0, 0, 0]))
+    # skel.q = np.random.rand(skel.ndofs) - 0.5
+    # skel.set_mobile(True)
+    # world.set_gravity([0, 9.8, 0])
     win = GLUTWindow(world, None)
     # win.scene.set_camera(None)
     win.scene.add_camera(
@@ -63,15 +47,9 @@ if __name__ == '__main__':
             trans=[0, 0.2, -.500]),
         "Camera Y up")
     win.scene.set_camera(2)
-    skel.bodynodes[0].set_mass(1)
+    skel.controller = DampingController(skel)
+    print(skel.ndofs)
     win.run()
 
-    pydart.gui.viewer.launch(world)
-    while world.t < 2.0:
+    # pydart.gui.viewer.launch(world)
 
-        print(world.nframes)
-        if world.nframes % 100 == 0:
-            stop = True
-            skel = world.skeletons[-1]
-            print("%.4fs: The last cube COM = %s" % (world.t, str(skel.C)))
-        world.step()
